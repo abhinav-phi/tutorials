889cd1590434f06790f2bcd1524dec284b8db81d
Abhinav <alpha9coder@gmail.com>
2025-06-14 23:50:41 +0530

    Add files via upload

---
+++ javascript/01basics/02datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert("abhinav")
+// we r  using nodejs not browser
+// semicolon lagao ya nhi, kuch fark nhi pdta
+
+console.log(3+3);
+console.log("abhinav");
+
+let name = "yadavji"
+let age = 18
+let isLoggedin = false
+
+// number ki range (max value = 2 to the power 53)
+// bigint
+// String -""
+// Boolean - true/false
+// null - standalone value
+// undefined
+// Symbol - unique
+
+// typeof null is Object
+
+console.log(typeof "abhinav");
+console.log(typeof null);
+console.log(typeof undefined);
