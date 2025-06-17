8a0c12c312ff45e016a2041d941005b1d56919fd
Abhinav <alpha9coder@gmail.com>
2025-06-17 12:00:40 +0530

    Add files via upload

---
+++ javascript/01basics/04operations.js
@@ -0,0 +1,50 @@
+let value =3
+let negvlaue = -value
+console.log(negvlaue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(15/2);
+console.log(15%2);
+
+let str1 = "hello"
+let str2 = "abhinav"
+let str3 = str1+" "+str2
+let str4 = str1+str2
+console.log(str3);
+console.log(str4);
+
+console.log( "1" + 2 ); //12
+console.log( 1 + "2" ); //12
+console.log( 1 + "2" + 2);  //122
+console.log( "1" + 2 + 2);  //122
+console.log( 1 + 2 + "2");  //32
+console.log( 1 + 2 + "2" + 2);  //322
+console.log( 1 + "2" +2 +2);  //1222
+
+// jaha pr string aaye,, uske baad se sab string addition rulw follow krenge
+
+console.log(true); //true
+console.log(+true); //1
+// console.log(true+); //error
+console.log(+""); //0
+
+// plus (+) likhne se conversion hojata h
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+ 
