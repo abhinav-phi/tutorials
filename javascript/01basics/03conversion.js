ae4643fbcf74f481805b6d0fa2143a101441c856
Abhinav <alpha9coder@gmail.com>
2025-06-17 12:00:18 +0530

    Add files via upload

---
+++ javascript/01basics/03conversion.js
@@ -0,0 +1,53 @@
+let score1 = "33"
+let score2 = "21abc"
+let score3 =  null
+let score4 =  undefined
+let score5 =  "abhinav"
+let score6 =  true
+
+// 2 tarike h typeof nikalne ke:
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueinnumber1 = Number(score1)
+let valueinnumber2 = Number(score2)
+let valueinnumber3 = Number(score3)
+let valueinnumber4 = Number(score4)
+let valueinnumber5 = Number(score5)
+let valueinnumber6 = Number(score6)
+
+console.log(typeof valueinnumber1);
+console.log(typeof valueinnumber2);
+console.log(typeof valueinnumber3);
+console.log(typeof valueinnumber4);
+console.log(typeof valueinnumber5);
+console.log(typeof valueinnumber6);
+
+console.log(valueinnumber1);
+console.log(valueinnumber2);
+console.log(valueinnumber3);
+console.log(valueinnumber4);
+console.log(valueinnumber5);
+console.log(valueinnumber6);
+
+let isLoggedIn1 = "abhinav"
+let isLoggedIn2 = ""
+
+let booleanIsLoggedin1 = Boolean(isLoggedIn1)
+let booleanIsLoggedin2 = Boolean(isLoggedIn2)
+console.log(booleanIsLoggedin1);
+console.log(booleanIsLoggedin2);
+
+//  1 => true, 0=> false
+// "xyz" => true, "" =>false
+
+let somenumber =23
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
+ 
