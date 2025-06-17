269be3f187ee690544e03b711354045de8fe1e44
Abhinav <alpha9coder@gmail.com>
2025-06-17 12:03:40 +0530

    Add files via upload

---
+++ javascript/01basics/05comparison.js
@@ -0,0 +1,26 @@
+console.log(2>1); // ans would be either true or false 
+console.log(2<1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2 != 1);
+
+
+console.log( "2" > 1); // true
+console.log("02" > 1); //true
+
+// javascript automatically converts string to number while doing comparison
+
+console.log( null > 0); // false
+console.log( null == 0); // false
+console.log( null >= 0); // true
+
+console.log( undefined > 0); // false
+console.log( undefined == 0); // false
+console.log( undefined >= 0); // flase
+
+// ===
+
+console.log("2" === 2); // triple equalto me conversion nhi hoti
+// console.log("2" == 2); double equalto convert kr deta h string ko number, datatype change kr leta h
+//  == -> loose equality
+// === -> strict equality
