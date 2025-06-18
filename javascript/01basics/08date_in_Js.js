03b0c6e78abcf40a7a13a601aa57a9702709e459
Abhinav <alpha9coder@gmail.com>
2025-06-18 06:08:23 +0530

    Update 08date_in_Js.js

--- javascript/01basics/08date_in_Js.js
+++ javascript/01basics/08date_in_Js.js
@@ -1,18 +1,17 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
 
-/* 
 
 let myCreatedDate = new Date(2023, 0, 23)
 let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 let myCreatedDate = new Date("2023-01-14")
-
-###in sab me month ki counting teri 0 se swtart hoti h
+/*
+###in sab me month ki counting teri 0 se start hoti h
 0 to 11 -> jan to dec
 
 */
@@ -21,13 +20,13 @@ let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
 // isme tera month ki counting 1 se count hoti h
 
-// console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
 console.log(newDate);
